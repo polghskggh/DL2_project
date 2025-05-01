3a3713363084bd0c11631fad8de029fe0ef4c97e
Tyme Chatupanyachotikul <122781615+tymechatupanyachotikul@users.noreply.github.com>
2025-05-01 10:19:14 +0200

    add log lines for debug

--- eeg/eeg_otta/tta/energy_adaptation.py
+++ eeg/eeg_otta/tta/energy_adaptation.py
@@ -71,6 +71,10 @@ class EnergyAdaptation(TTAMethod):
         init_samples = deepcopy(init_sample)
         x_k = torch.autograd.Variable(init_sample, requires_grad=True)
         # sgld
+        print(f'model device : {next(self.energy_model.parameters()).device}')
+        print(f'x device : {x_k.device}')
+        print(f'y device: {y.device}')
+
         for k in range(n_steps):
             f_prime = torch.autograd.grad(self.energy_model(x_k, y=y)[0].sum(), [x_k], retain_graph=True)[0]
             x_k.data += sgld_lr * f_prime + sgld_std * torch.randn_like(x_k)
